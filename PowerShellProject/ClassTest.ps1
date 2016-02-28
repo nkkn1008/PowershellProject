c51906336a6b1c71ca6ad61e5fa400c327a7ebfc
Kenichi Nakamura <nkkn1008@gmail.com>
2016-02-29 07:18:20 +0900

    Initialize Powershell Project

---
+++ PowerShellProject/ClassTest.ps1
@@ -0,0 +1,32 @@
+﻿#
+# ClassTest.ps1
+#
+
+function Main()
+{
+	$student = [Student]::New();
+}
+
+class Person
+{
+	[string]$Name;
+	[int]$Age;
+
+	# Contructor
+	Person()
+	{
+		Write-Host "Base Constructor"
+	}
+}
+
+class Student : Person
+{
+	[int]$Id;
+
+	Student()
+	{
+		Write-Host "Derived constructor called"
+	}
+}
+
+Main
